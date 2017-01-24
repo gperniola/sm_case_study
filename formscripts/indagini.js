b2261a41cc70d0482068379024182ad5be66449f
Giuseppe Perniola <g.perniola22@gmail.com>
2017-01-24 10:25:47 +0100

    added indagini.js

---
+++ formscripts/indagini.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+	
+	
+	
+	
+	
+
+	$('#concludi').prop('disabled',true);
+	$('#annulla').prop('disabled',true);
+	
+	$('#nomeCp').prop('disabled',true);
+	$('#cognomeCp').prop('disabled',true);
+	
+    $("#nuovoFile").click(function(){
+		
+        $("#formIndagini").show(200);
+		$('#nuovoFile').prop('disabled',true);
+		$('#concludi').prop('disabled',false);
+		$('#annulla').prop('disabled',false);
+    });
+	
+	$("#concludi").click(function(){
+	
+		var tipo = $("#tipoIndagine").val();
+		var data = $("#data").val();
+		var referto = $("#referto").val();
+		var allegato = $("#allegato").val();
+		
+		
+		if(tipo.trim()=='' || data.trim()=='' || referto.trim()=='' || allegato.trim()==''){
+			alert('Tutti i campi sono obbligatori.');
+		}else{
+		
+	$("#formIndagini").hide(200);
+		$('#nuovoFile').prop('disabled',false);
+		$('#concludi').prop('disabled',true);
+		$('#annulla').prop('disabled',true);
+		
+		
+		
+		$.post("formscripts/nuovaIndagine.php",
+			{	
+			
+				idPaziente:  $("#idPaziente").val(),
+				idDiagnosi:  $("#idDiagnosi").val(),
+				motivo:      $("#motivoIndagine").val(),
+				tipo:        $("#tipoIndagine").val(),
+				data:        $("#data").val(),
+				referto:     $("#referto").val(),
+				allegato:    $("#allegato").val(),
+				
+			},
+			function(status){
+				$('#formIndagini')[0].reset();
+    			//alert("Status: " + status);
+				$('#tableIndagini').append('<tr><td>'+data+'</td><td>'+tipo+'</td><td>'+referto+'</td><td>'+allegato+'</td></tr>');
+  			});
+		
+		
+		}
+		
+
+
+        
+    });
+	
+	$("#annulla").click(function(){
+		
+        $("#formIndagini").hide(200);
+		$('#nuovoFile').prop('disabled',false);
+		$('#concludi').prop('disabled',true);
+		$('#annulla').prop('disabled',true);
+    });
+	
+	
+});
+
+
+
+
+

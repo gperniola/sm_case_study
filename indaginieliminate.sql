ac4b073ac17703089bb0efbdb97692736501a7d7
Giuseppe Perniola <g.perniola22@gmail.com>
2017-02-24 19:54:50 +0100

    Fixed auto_increment

--- indaginieliminate.sql
+++ indaginieliminate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2017 at 06:49 PM
+-- Generation Time: Feb 24, 2017 at 06:54 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -43,6 +43,15 @@ CREATE TABLE `indaginieliminate` (
 ALTER TABLE `indaginieliminate`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `indaginieliminate`
+--
+ALTER TABLE `indaginieliminate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
